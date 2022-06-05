b0264b07520efda023f9b1536f49eb9abced37bc
Hrvoje Kristić <hk51645@fer.hr>
2022-06-06 00:37:54 +0200

    popravak

--- dodatak_A.py
+++ dodatak_A.py
@@ -30,19 +30,13 @@ class OperationsManager():
 
 
 if __name__ == "__main__":
-    user = input("Username: ")
-    password = getpass.getpass("Password: ")
-    if user != "root" or password != "123":
-        print("Wrong username or password!")
-        exit(0)
-    else:
-        print("Login success!")
-        a = float(input("A = "))
-        b = float(input("B = "))
-        ops_manager = OperationsManager(a, b)
-        print(ops_manager.perform_division())
-        print(ops_manager.square_binomial())
-        print(ops_manager.pythagorean_rule())
-        print(ops_manager.factorial_sum())
-        print(ops_manager.circle_area())
+    print("Login success!")
+    a = float(input("A = "))
+    b = float(input("B = "))
+    ops_manager = OperationsManager(a, b)
+    print(ops_manager.perform_division())
+    print(ops_manager.square_binomial())
+    print(ops_manager.pythagorean_rule())
+    print(ops_manager.factorial_sum())
+    print(ops_manager.circle_area())
 
